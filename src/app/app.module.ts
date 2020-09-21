2be861404e621ce92e86c93f81f00651d25cdb85
Chathurajay <chathurajayz94@gmail.com>
2020-09-21 22:16:37 +0530

    My First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
+import { WhyComponent } from './why/why.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    HeaderComponent,
+    AboutusComponent,
+    ServiceComponent,
+    WhyComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
